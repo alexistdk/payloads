c0ac932e5d173176eebb121f3613a3de42f3cbc9
alexistdk <alexisndelgado@gmail.com>
2021-02-05 20:56:24 -0300

    first commit

---
+++ package/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='bb_payload',
+    verion='0.1',
+    description='Payloads necesarios para los scrapings',
+    url='https://github.com/alexistdk/payloads',
+    author='Alexis Delgado',
+    author_email='ad@bb.vision',
+    packages=['payload', 'time', 'datetime']
+)
