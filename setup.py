8042d42898f8c5c16f398a3e5b53f172838f5823
alexistdk <alexisndelgado@gmail.com>
2021-02-05 21:39:06 -0300

    mejorada la librería

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='bb',
+    verion='0.2',
+    description='Payloads necesarios para los scrapings',
+    url='https://github.com/alexistdk/payloads',
+    author='Alexis Delgado',
+    author_email='ad@bb.vision',
+    packages=['bb'],
+    install_requires=['datetime'],
+    zip_safe=False
+)
